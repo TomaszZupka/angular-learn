7366e64679c40dec4606c76bd55db378e34c3326
Tomasz Zupka <tomasz.zupka@gmail.com>
2017-09-25 13:20:10 +0200

    component setup

---
+++ src/app/schopping-list/schopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schopping-list',
+  templateUrl: './schopping-list.component.html',
+  styleUrls: ['./schopping-list.component.css']
+})
+export class SchoppingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
