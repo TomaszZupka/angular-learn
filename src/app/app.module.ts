fa03dba9d2ea9280f5e19b18ba4fd37787d35be3
Tomasz Zupka <tomasz.zupka@gmail.com>
2017-09-25 13:23:21 +0200

    setup part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { SchoppingListComponent } from './schopping-list/schopping-list.component';
-import { ShoppingEditComponent } from './schopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { ShoppingEditComponent } from './schopping-list/shopping-edit/shopping-e
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
-    SchoppingListComponent,
+    ShoppingListComponent,
     ShoppingEditComponent
   ],
   imports: [
