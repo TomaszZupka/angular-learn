80135ec3e3a9394498009e6e553be16bc1a4a61c
Tomasz Zupka <tomasz.zupka@gmail.com>
2017-09-28 16:57:34 +0200

    ingredients service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
-export class ShoppingListService {
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
 
+export class ShoppingListService {
+  ingedientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('tomatoes', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingedientsChanged.emit(this.ingredients.slice());
+  }
 }
